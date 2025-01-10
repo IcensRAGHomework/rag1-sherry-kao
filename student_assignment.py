000b3c456d666ef6c73fdf7a9b4e580515a63b58
sherry-kao <sherry_kao@asus.com>
2025-01-10 10:15:18 +0800

    upload test

--- student_assignment.py
+++ student_assignment.py
@@ -6,11 +6,41 @@ from model_configurations import get_model_configuration
 from langchain_openai import AzureChatOpenAI
 from langchain_core.messages import HumanMessage
 
+import re
+from langchain_core.output_parsers import JsonOutputParser
+
 gpt_chat_version = 'gpt-4o'
 gpt_config = get_model_configuration(gpt_chat_version)
 
 def generate_hw01(question):
-    pass
+    llm = AzureChatOpenAI(
+            model=gpt_config['model_name'],
+            deployment_name=gpt_config['deployment_name'],
+            openai_api_key=gpt_config['api_key'],
+            openai_api_version=gpt_config['api_version'],
+            azure_endpoint=gpt_config['api_base'],
+            temperature=gpt_config['temperature']
+    )
+    
+    match = re.search(r'(\d{4})年台灣(\d{1,2})月', question)
+    if match:
+        year = match.group(1)
+        month = match.group(2).zfill(2) 
+        #print(year)
+        #print(month)
+        prompt = f"列出{year}年台灣{month}月的所有紀念日，並以JSON格式呈現，每個紀念日包含日期和名稱，例如：{{'date': '年份-月份-日期', 'name': '紀念日名稱'}}。"
+        response = llm(prompt)
+
+        json_parser = JsonOutputParser()
+        json_output = json_parser.invoke(response)
+        result = {"Result": json_output}
+    else:
+        result = {"Result": []}
+
+
+    print(result)
+    return result
+    #pass
     
 def generate_hw02(question):
     pass
